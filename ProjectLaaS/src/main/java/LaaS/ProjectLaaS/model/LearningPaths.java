8a0bef378b8e2126979793b76dd7cfcfd103eed6
Willem van der Hoeven <willemvdhoeven@MacBook-Air-32.local>
2024-07-29 10:01:24 +0200

    update

--- ProjectLaaS/src/main/java/LaaS/ProjectLaaS/model/LearningPaths.java
+++ ProjectLaaS/src/main/java/LaaS/ProjectLaaS/model/LearningPaths.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class LearningPaths {
@@ -19,7 +20,7 @@ public class LearningPaths {
 	private String learningPathName;
 	private int userId;
 	
-	@ManyToAny
+	@ManyToMany
 	private List <Course> Courses = new ArrayList();	
 	
 	
