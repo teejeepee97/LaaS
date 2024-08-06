5794d68470111f9002c7f80a09ecef0218ee00b3
BvanIngen <bryan.ingen@gmail.com>
2024-08-06 12:02:59 +0200

    hopen dat dit werkt

--- ProjectLaaS/src/main/java/LaaS/ProjectLaaS/config/SimpleCORSFilter.java
+++ ProjectLaaS/src/main/java/LaaS/ProjectLaaS/config/SimpleCORSFilter.java
@@ -1,3 +1,23 @@
+//package LaaS.ProjectLaaS.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class SimpleCORSFilter implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")  // Add your frontend origin(s) here
+//                .allowedMethods("GET", "POST", "PUT", "DELETE")
+//        		.allowedHeaders("*")
+//        		.allowCredentials(true);  // Allow credentials (cookies, authorization headers, etc.)
+//
+//    }
+//}
+
 package LaaS.ProjectLaaS.config;
 
 import org.springframework.context.annotation.Configuration;
--- ProjectLaaS/src/main/java/LaaS/ProjectLaaS/rest/InlogEndpoint.java
+++ ProjectLaaS/src/main/java/LaaS/ProjectLaaS/rest/InlogEndpoint.java
@@ -2,7 +2,7 @@ package LaaS.ProjectLaaS.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,14 +19,27 @@ public class InlogEndpoint {
 	private final InlogService inlogService = new InlogService();
 
 	
-	@GetMapping("/login")
-    public String login(@RequestParam String username, @RequestParam String password) {
-        if (inlogService.authenticate(username, password)) {
-            return "Login successful!";
-        } else {
-            return "Invalid username or password.";
-        }
-    }
+//	@GetMapping("/login")
+//    public String login(@RequestParam String username, @RequestParam String password) {
+//        if (inlogService.authenticate(username, password)) {
+//            return "Login successful!";
+//        } else {
+//            return "Invalid username or password.";
+//        }
+//    }
+	
+	@GetMapping("/login/{username}/{password}")
+    public String login(@PathVariable String username, @PathVariable String password) {
+		try {
+	        if (inlogService.authenticate(username, password)) {
+	            return "Login successful!";
+	        } else {
+	            return "Invalid username or password.";
+	        }
+		} catch(Exception e) {
+			return "Unknown username";
+		}
+	}
 	
 }
 
