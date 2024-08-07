30ce9c648d61e6529c9d47c8683788f0484648e9
BvanIngen <bryan.ingen@gmail.com>
2024-08-07 11:32:02 +0200

    Verandering aan CORS filter origins

--- ProjectLaaS/src/main/java/LaaS/ProjectLaaS/config/SimpleCORSFilter.java
+++ ProjectLaaS/src/main/java/LaaS/ProjectLaaS/config/SimpleCORSFilter.java
@@ -30,10 +30,9 @@ public class SimpleCORSFilter implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")  // Add your frontend origin(s) here
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-        		.allowedHeaders("*")
-        		.allowCredentials(true);  // Allow credentials (cookies, authorization headers, etc.)
-
+                .allowedOrigins("http://127.0.0.1:5500", "https://red-smoke-07c837103.5.azurestaticapps.net") // Add your frontend origin(s) here
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);  // Allow credentials (cookies, authorization headers, etc.)
     }
 }
